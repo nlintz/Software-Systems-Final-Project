a2eb9a521c414b7c7e026e0e0ad629a8aafacd00
Nathan Lintz <nlintz@gmail.com>
2014-04-28 11:53:25 -0400

    softsys final

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Nathan Lintz on 4/22/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode <CCPhysicsCollisionDelegate>
+
+@end
